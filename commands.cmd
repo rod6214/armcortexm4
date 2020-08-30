98f5556dfe6595bf894a5589c4d250ef76e2ac14
Nelson Amador <rod6214@gmail.com>
2020-08-29 23:20:50 -0300

    Added rutine to turn led on

--- commands.cmd
+++ commands.cmd
@@ -1,5 +1,5 @@
 # Compile
-arm-none-eabi-as -mapcs-32 -mcpu=cortex-m4 -mthumb -o program.o main.S
+arm-none-eabi-as -mapcs-32 -mcpu=cortex-m4 -march=armv7 -o program.o main.S
 # Link
 arm-none-eabi-ld -T linker -o program.elf program.o
 arm-none-eabi-ld -Ttext=0x80001ac -TVectorTable=0x8000000 -o program.elf program.o
